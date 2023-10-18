ee80b8ec1a089657debb6077cd5c2ee5b8ba652c
Daniel Sinderson <drsinderson@gmail.com>
2023-10-18 07:21:19 -0700

    added decision_matrix.py skeleton

---
+++ assistant_app/decision_matrix.py
@@ -0,0 +1,29 @@
+"""
+
+"""
+
+default_criteria: list[tuple[str, float]] = [
+    ("pro-social goodness", 1.0),
+    ("intrinsic goodness", 1.0),
+    ("money money money", 1.0)
+]
+
+
+def ask_for_criteria() -> list[tuple[str, float]]:
+    pass
+
+
+def ask_for_options() -> list[tuple[str, float]]:
+    pass
+
+
+def calculate_results(criteria, options) -> dict:
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
