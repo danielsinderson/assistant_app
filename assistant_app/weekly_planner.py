39c1c9842d6f2f67bc5104b7ca51213b708c5ac7
Daniel Sinderson <drsinderson@gmail.com>
2023-10-18 05:39:31 -0700

    finished first draft of weekly planner

--- assistant_app/weekly_planner.py
+++ assistant_app/weekly_planner.py
@@ -4,6 +4,7 @@
 import os, sys, yaml, random
 from datetime import datetime
 from datetime import timedelta
+from datetime import date
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 data_dir = script_dir + '/data/'
@@ -16,8 +17,29 @@ notebook_path: str = planner_data['notebook_path']
 
 
 def format_appointments_and_events() -> str:
+    def upcoming(d: str) -> bool:
+        today = date.today()
+        
+        month = int(d.split('-')[0])
+        day = int(d.split('-')[1])
+        year = today.year if (month >= today.month) else today.year + 1
+        event_date = date(year, month, day)
+        
+        difference = event_date - today
+        # print(month, day, year, difference)
+        if 0 <= difference.days <= 14:
+            return True
+        else:
+            return False
+    
     appointments: dict = planner_data['appointments']
-    today = datetime.today()
+    upcoming_appointments = [f"{key}: {appointments[key]}" for key in appointments.keys() if upcoming(appointments[key].split()[0])]
+
+    events: dict = planner_data['events']
+    upcoming_events = [f"{key}: {events[key]}" for key in events.keys() if upcoming(events[key])]
+
+    output = f"## Calendar\n" + "**Appointments**\n" + "\n".join(upcoming_appointments) + "\n\n**Events**\n" + "\n".join(upcoming_events)
+    return output
 
 
 
@@ -39,7 +61,11 @@ def format_maintenance() -> str:
 
 def create_note() -> None:
     maintenance: str = format_maintenance()
-    print(maintenance)
+    calendar: str = format_appointments_and_events()
+    output: str = "\n" + calendar + "\n" + maintenance
+    #print(output)
+    with open(notebook_path + "Weekly Planner.md", 'w') as planner_note_file:
+        planner_note_file.write(f"Weekly Planner for the week of {date.today()}:\n\n" + output)
 
 
 def main():
