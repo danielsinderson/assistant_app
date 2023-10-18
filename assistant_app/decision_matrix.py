b01eed117a710f819d2e12fbc76f9120b794e50d
Daniel Sinderson <drsinderson@gmail.com>
2023-10-18 08:34:25 -0700

    finished decision_matrix.py first draft

--- assistant_app/decision_matrix.py
+++ assistant_app/decision_matrix.py
@@ -10,19 +10,39 @@ default_criteria: list[tuple[str, float]] = [
 
 
 def ask_for_criteria() -> list[tuple[str, float]]:
-    pass
+    criteria = input("Please input your criteria in the following format \
+                     criteria2:weight2,criteria2:weight2,etc. \
+                     or press [enter] to use the default criteria:\n")
+    if criteria == "":
+        return default_criteria
+    
+    criteria_list = [(elem.split(':')[0], float(elem.split(':')[1])) for elem in criteria.split(',')]
+    print(criteria_list)
+    return criteria_list
 
 
-def ask_for_options() -> list[tuple[str, float]]:
-    pass
-
-
-def calculate_results(criteria, options) -> dict:
-    pass
+def ask_for_options(criteria: list) -> list[tuple[str, float]]:
+    results: dict[str, float] = {}
+    while True:
+        option_name = input("Type out your next option, or press [enter] to move on: ")
+        
+        if option_name == "":
+            return results
+        
+        total_score = 0
+        for criterion in criteria:
+            criterion_score = float(input(f"Please assign a score for {criterion[0]}: "))
+            total_score += criterion_score * criterion[1]
+        
+        results[option_name] = total_score
 
 
 def main():
-    pass
+    criteria: list[tuple[str, float]] = ask_for_criteria()
+    options: dict[str, float] = ask_for_options(criteria)
+    for option, value in options.items():
+        print(f"{option} has a score of {value}.")
+
 
 
 if __name__ == "__main__":
